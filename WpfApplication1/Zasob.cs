1ab665c53b17dad62a6cfe6018befe33a6963091
Wojciech <wojtekziomm@gmail.com>
2015-01-02 20:02:36 +0100

    dodane masowki

---
+++ WpfApplication1/Zasob.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stolowka
+{
+    class Zasob
+    {
+        string nazwa;
+
+        public string Nazwa
+        {
+            get { return nazwa; }
+            set { nazwa = value; }
+        }
+
+        double ilosc;
+
+        public double Ilosc
+        {
+            get { return ilosc; }
+            set { ilosc = value; }
+        }
+
+        double cenaJedn;
+
+        public double CenaJedn
+        {
+            get { return cenaJedn; }
+            set { cenaJedn = value; }
+        }
+    }
+}
