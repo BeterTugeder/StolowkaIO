736a88ff42e084b4f5e52b2c63925c00f37b5629
BeterTugeder <qba307@gmail.com>
2015-01-18 12:05:05 +0100

    Master

--- WpfApplication1/Uzytkownik.cs
+++ WpfApplication1/Uzytkownik.cs
@@ -15,5 +15,37 @@ namespace Stolowka
             get { return id; }
             set { id = value; }
         }
+
+        string imie;
+
+        public string Imie
+        {
+            get { return imie; }
+            set { imie = value; }
+        }
+
+        string nazwisko;
+
+        public string Nazwisko
+        {
+            get { return nazwisko; }
+            set { nazwisko = value; }
+        }
+
+        string login;
+
+        public string Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+
+        bool typ;
+
+        public bool Typ
+        {
+            get { return typ; }
+            set { typ = value; }
+        }
     }
 }
