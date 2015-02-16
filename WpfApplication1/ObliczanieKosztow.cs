91885a5744be871f4a80145959c6a4c444882e71
BeterTugeder <qba307@gmail.com>
2015-02-16 22:01:33 +0100

    Logowanie

---
+++ WpfApplication1/ObliczanieKosztow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stolowka
+{
+    class ObliczanieKosztow
+    {
+        public double kosztNaOsobe(DateTime dataOd, DateTime dataDo)
+        {
+            return 0.0;
+        }
+
+        public double kosztZaOkres(DateTime dataOd, DateTime dataDo)
+        {
+            return 0.0;
+        }
+    }
+}
